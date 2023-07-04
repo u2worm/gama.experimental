eeba341fb7a0edd4e28aa7482f0179da92907223
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2023-07-04 13:16:40 +0200

    :tada: Chemmisol plugin setup

---
+++ ummisco.gama.chemmisol/src/ummisco/gama/chemmisol/skills/ChemicalSkill.java
@@ -0,0 +1,9 @@
+package ummisco.gama.chemmisol.skills;
+
+import msi.gama.precompiler.GamlAnnotations.skill;
+import msi.gaml.skills.Skill;
+
+@skill(name=IChemicalSkill.SKILL_NAME)
+public class ChemicalSkill extends Skill {
+
+}
